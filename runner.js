a68827ad8d67f3482896878fd5420fc185434363
unknown <yaumashf@gmail.com>
2017-01-11 06:44:20 +0700

    race

--- runner.js
+++ runner.js
@@ -12,3 +12,21 @@ function sleep(milliseconds) {
 }
 
 // Your code here...
+let players = [{name:'a',pos : 0},{name:'b',pos : 0},{name:'c',pos : 0}]
+const runner = new JSRacer(players)
+for (let i=0; i<30; i++){
+	runner.print_board();
+	sleep(300)
+	if(runner.players[0].pos > 29 || runner.players[1].pos > 29 ||runner.players[2].pos > 29){
+          // runner.setPos();
+          // runner.reset_board();
+          runner.finished();
+          break;
+        }
+        runner.reset_board();
+}
+//        runner.reset_board();
+// runner.finished();
+for(let i=0; i<players.length; i++){
+	console.log('position '+players[i].name+': '+players[i].pos)
+}
