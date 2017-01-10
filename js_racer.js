4574d7d9e47fa549503f1fa8631050e076062679
Irsan Sebastian <email": null>
2017-01-10 21:11:58 +0700

    first commit

--- js_racer.js
+++ js_racer.js
@@ -4,7 +4,7 @@ import Dice from "./dice.js"
 
 class JSRacer {
   constructor(players, length) {
-    this.players = [{name: 'a',pos:0}, {name: 'b', pos: 0}];
+    this.players = [{name: 'a',pos : 0}, {name: 'b', pos : 0}];
 
     this.length = length || 20;
 
@@ -14,41 +14,54 @@ class JSRacer {
   print_board() {
     for(let i = 0; i < this.players.length ;i++){
 
+
       this.print_line(this.players[i]);
       this.advanced_player(this.players[i]);
 
+
     }
+
 
   }
   print_line(player) {
     let arr = [];
     for(let i = 0; i < this.length ;i++){
+
+
           if(player.pos == i){
-              arr.push(player.name)
-          }else{
+              arr.push(player.name);
+
+
+          }//terakhir sampai disini
+              else{
               arr.push(" ");
           }
+
+
     }
     console.log(arr.join("|"));
+    if(player.pos == this.length)
+         arr.push(player.name);
+           return "Winner ; "+player.name;
+
+
   }
 
     advanced_player(player) {
       // player = {name: 'a', pos: 0}
-      player.pos += Dice.roll()
+      player.pos += Dice.roll();
     }
 
-    finished() {
-
-
-    }
-
-  winner() {
-
-  }
+  //   winner(player){
+  //
+  //         if(player.pos == (this.length - 1))
+  //
+  //
+  // }
 
 
   reset_board() {
-    console.log("\x1B[2J")
+    console.log("\x1B[2J");
   }
 }
 
