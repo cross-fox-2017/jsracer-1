0b081d31be9c51d59aa16c82a7ba584eb771db73
Syanmil <syanmil@yahoo.com>
2017-01-11 16:28:55 +0700

    controller terpisah

--- runner.js
+++ runner.js
@@ -2,13 +2,8 @@
 
 import JSRacer from "./js_racer"
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
-  }
-}
-
 // Your code here...
+var Berkuda = new JSRacer(["A", "B", "C"], 20, 6)
+
+console.log(Berkuda.startingBoard());
+console.log(Berkuda.searchWinner());
