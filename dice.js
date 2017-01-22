00c7dcd48b08d04cb2c75a701fd7ba42f86f2985
irwinpratajaya <irwinpratajaya@gmail.com>
2017-01-23 02:47:32 +0700

    done

--- dice.js
+++ dice.js
@@ -1,12 +1,16 @@
 "use strict"
 
-class Dice {
-  constructor() {
-
-  }
-  roll() {
-    
-  }
+export function move(players){
+    var a= Math.floor((Math.random()*6)+1);
+    if(players.pos+a>29){
+      players.trek.splice(29,1,players.nama);
+      players.trek.splice(players.pos,1," ")
+      players.pos=29
+    }
+    else{
+      players.trek.splice(a+players.pos,1,players.nama);
+      players.trek.splice(players.pos,1," ")
+      players.pos= a+players.pos;
+    }
+      return players.trek.join('|')
 }
-
-export default Dice
